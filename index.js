2a625601664b10a0b578bc04fe8e55c9e2a950cf
yabo <yarbrough.lu@gmail.com>
2018-01-02 19:28:04 +0800

    load more

---
+++ index.js
@@ -0,0 +1,63 @@
+function ajax(opts){
+	var url = opts.url;
+	var type = opts.type || 'GET';
+	var dataType = opts.dataType || 'json';
+	var onsuccess = opts.onsuccess || function(){};
+	var onerror = opts.onerror || function(){};
+	var data = opts.data || {};
+
+	var dataStr = [];
+	for(var key in data){
+		dataStr.push(key + '=' + data[key]);
+	}
+	dataStr = dataStr.join('&');
+
+	if(type === 'GET'){
+		url += '?' + dataStr;
+	}
+
+	var xhr = new XMLHttpRequest();
+	xhr.open(type, url, true);
+	xhr.onload = function(){
+		if((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304){
+			if(dataType === 'json'){
+				onsuccess(JSON.parse(xhr.resonseText));
+			}else{
+				onsuccess(xhr.responseText);
+			}
+		}else {
+			onerror = onerror();
+		}
+	}
+
+	if(type === 'POST'){
+		xhr.send(dataStr);
+	}else {
+		xhr.send();
+	}
+}
+
+var i = 1;
+
+loadMore.addEventListener('click', function(){
+  var url = '/page' + i + '.json';
+
+  var onsuccess = function(result){
+  	var ul = document.querySelector('.content-list');
+  	for(var i=0;i<result.length; i++){
+  		var li = document.createElement('li');
+  		li.contentText = result[i];
+  		ul.appendChild(li);
+  	}
+  }
+
+  var opts = {
+  	onsuccess: onsuccess, 
+  	url: url
+  }
+
+  ajax(opts);
+
+
+
+});

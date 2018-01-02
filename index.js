7cc704389a21f13338fd868fa5a545f070b67fc0
yabo <yarbrough.lu@gmail.com>
2018-01-02 21:56:13 +0800

    调整js结构

--- index.js
+++ index.js
@@ -1,64 +1,37 @@
-function ajax(opts){
-	var url = opts.url;
-	var type = opts.type || 'GET';
-	var dataType = opts.dataType || 'json';
-	var onsuccess = opts.onsuccess || function(){};
-	var onerror = opts.onerror || function(){};
-	var data = opts.data || {};
 
-	var dataStr = [];
-	for(var key in data){
-		dataStr.push(key + '=' + data[key]);
-	}
-	dataStr = dataStr.join('&');
 
-	if(type === 'GET'){
-		url += '?' + dataStr;
-	}
-
-	var xhr = new XMLHttpRequest();
-	xhr.open(type, url, true);
-	xhr.onload = function(){
-		if((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304){
-			if(dataType === 'json'){
-				onsuccess(JSON.parse(xhr.responseText));
-			}else{
-				onsuccess(xhr.responseText);
-			}
-		}else {
-			onerror = onerror();
-		}
-	}
-
-	if(type === 'POST'){
-		xhr.send(dataStr);
-	}else {
-		xhr.send();
-	}
-}
-
-var i = 1;
+var index = 1;
+var isRequest = false;
 
 loadMore.addEventListener('click', function(){
-  var url = './page' + i + '.json';
+	if(!isRequest){
+		isRequest = true;
+		var url = './page' + index + '.json';
+		var xhr = new XMLHttpRequest();
 
-  var onsuccess = function(result){
-  	var ul = document.querySelector('.content-list');
-  	for(var i=0;i<result.length; i++){
-  		var li = document.createElement('li');
-  		console.log(result[i]);
-  		li.textContent = result[i];
-  		ul.appendChild(li);
-  	}
-  }
+		xhr.open('GET', url, true);
+	  	xhr.onload = function(){
+	  		isRequest = false;
+			if((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304){
+				var result = JSON.parse(xhr.responseText);
+				var hasNext = result.hasNext;
+				var data = result.data;
+				var ul = document.querySelector('.content-list');
 
-  var opts = {
-  	onsuccess: onsuccess, 
-  	url: url
-  }
+				for(var i=0; i<data.length; i++){
+					var li = document.createElement('li');
+					li.textContent = data[i];
+					ul.appendChild(li);
+				}
+				if(hasNext){
+					index++;
+				}else {
+					loadMore.disabled = true;
+				}
 
-  ajax(opts);
+			}
+		}
 
-
-
+		xhr.send();
+	}
 });
